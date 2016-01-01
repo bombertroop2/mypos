bb3a4a5900cd89236eb7b2e494fbea41ebc3c35f
Yopi Agung Saputra <bombertroop@gmail.com>
2016-01-01 17:24:58 +0700

    separate add foreign key function to prevent error when migrating on Heroku

--- db/migrate/20151221095839_create_warehouses.rb
+++ db/migrate/20151221095839_create_warehouses.rb
@@ -6,10 +6,11 @@ class CreateWarehouses < ActiveRecord::Migration
       t.text :address
       t.boolean :is_active
       t.references :supervisor, index: true, foreign_key: true
-      t.references :region, index: true, foreign_key: true
+      t.references :region, index: true
       t.string :warehouse_type
 
       t.timestamps null: false
     end
+    add_foreign_key :warehouses, :common_fields, column: :region_id
   end
 end
