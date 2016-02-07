9a7396458bbaa2fd14f6f6cca1c33a02af46bed2
Yopi Agung Saputra <bombertroop@gmail.com>
2016-02-07 17:24:02 +0700

    modified db/migrate/20160126112502_create_received_purchase_orders.rb

--- db/migrate/20160126112502_create_received_purchase_orders.rb
+++ db/migrate/20160126112502_create_received_purchase_orders.rb
@@ -2,9 +2,10 @@ class CreateReceivedPurchaseOrders < ActiveRecord::Migration
   def change
     create_table :received_purchase_orders do |t|
       t.references :purchase_order_product, index: true, foreign_key: true
-      t.references :color, index: true, foreign_key: true
+      t.references :color, index: true
 
       t.timestamps null: false
     end
+    add_foreign_key :received_purchase_orders, :common_fields, column: :color_id
   end
 end
