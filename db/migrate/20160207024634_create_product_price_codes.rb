1d3b4afe3172031c55348caae23cb83b07a96311
Yopi Agung Saputra <bombertroop@gmail.com>
2016-02-07 17:27:47 +0700

    modified db/migrate/20160207024634_create_product_price_codes.rb

--- db/migrate/20160207024634_create_product_price_codes.rb
+++ db/migrate/20160207024634_create_product_price_codes.rb
@@ -2,9 +2,10 @@ class CreateProductPriceCodes < ActiveRecord::Migration
   def change
     create_table :product_price_codes do |t|
       t.references :product, index: true, foreign_key: true
-      t.references :price_code, index: true, foreign_key: true
+      t.references :price_code, index: true
 
       t.timestamps null: false
     end
+    add_foreign_key :product_price_codes, :common_fields, column: :price_code_id
   end
 end
