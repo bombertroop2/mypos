400dfd47f2f5f72c7452e80be475f7fbed040df1
Yopi Agung Saputra <bombertroop@gmail.com>
2016-03-27 14:06:38 +0700

    added function for preventing update code if dependent product exist on color module

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -66,12 +66,20 @@ class ColorsController < ApplicationController
   def destroy
     @color.destroy
     if @color.errors.present? and @color.errors.messages[:base].present?
-      message = @color.errors.messages[:base].to_sentence
+      error_message = @color.errors.messages[:base].to_sentence
+      error_message.slice! "details "
+      alert = error_message
     else
-      message = 'Color was successfully destroyed.'
+      notice = 'Color was successfully deleted.'
     end
     respond_to do |format|
-      format.html { redirect_to colors_url, notice: message }
+      format.html do
+        if notice.present?
+          redirect_to colors_url, notice: notice
+        else
+          redirect_to colors_url, alert: alert
+        end
+      end
       format.json { head :no_content }
     end
   end
--- app/models/color.rb
+++ app/models/color.rb
@@ -1,17 +1,20 @@
 class Color < CommonField
-  has_many :product_details, dependent: :restrict_with_error
+  has_many :purchase_order_details, dependent: :restrict_with_error
   #  has_many :received_purchase_orders
   has_many :products, -> {group "products.id"}, through: :product_details
 
-  before_validation :titleize_name, :upcase_code
+  before_validation :upcase_code
 
   validates :code, uniqueness: true
-
-  def titleize_name
-    self.name = name.titleize
-  end
+  validate :code_not_changed
+  
+  private
 
   def upcase_code
     self.code = code.upcase
+  end
+  
+  def code_not_changed
+    errors.add(:code, "change is not allowed!") if code_changed? && persisted? && purchase_order_details.present?
   end
 end
