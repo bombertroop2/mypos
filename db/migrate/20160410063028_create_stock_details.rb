2aeff142e764d950339f72615fecc18f4b904bb1
Yopi Agung Saputra <bombertroop@gmail.com>
2016-04-10 19:15:33 +0700

    change create stock details migration file

--- db/migrate/20160410063028_create_stock_details.rb
+++ db/migrate/20160410063028_create_stock_details.rb
@@ -3,10 +3,11 @@ class CreateStockDetails < ActiveRecord::Migration
     create_table :stock_details do |t|
       t.references :stock_product, index: true, foreign_key: true
       t.references :size, index: true, foreign_key: true
-      t.references :color, index: true, foreign_key: true
+      t.references :color, index: true
       t.integer :quantity
 
       t.timestamps null: false
     end
+    add_foreign_key :stock_details, :common_fields, column: :color_id
   end
 end
