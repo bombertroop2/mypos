064eb31e08e299f24c9ca76eaf5847824ab6bb67
Yopi Agung Saputra <bombertroop@gmail.com>
2016-04-20 07:51:30 +0700

    add color_id foreign key to DirectPurchaseDetail

--- db/migrate/20160418153510_create_direct_purchase_details.rb
+++ db/migrate/20160418153510_create_direct_purchase_details.rb
@@ -5,10 +5,11 @@ class CreateDirectPurchaseDetails < ActiveRecord::Migration
       t.decimal :total_unit_price
       t.references :direct_purchase_product, index: true, foreign_key: true
       t.references :size, index: true, foreign_key: true
-      t.references :color, index: true, foreign_key: true
+      t.references :color, index: true
       t.integer :returning_qty
 
       t.timestamps null: false
     end
+    add_foreign_key :direct_purchase_details, :common_fields, column: :color_id
   end
 end
