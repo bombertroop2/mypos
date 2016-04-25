c6a7f75d14aa334fbe930d74260da29c15e682a2
Yopi Agung Saputra <bombertroop@gmail.com>
2016-04-25 07:31:42 +0700

    refactored some queries on region model

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -4,7 +4,7 @@ class RegionsController < ApplicationController
   # GET /regions
   # GET /regions.json
   def index
-    @regions = Region.all
+    @regions = Region.select :id, :code, :name
   end
 
   # GET /regions/1
@@ -85,7 +85,7 @@ class RegionsController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_region
-    @region = Region.find(params[:id])
+    @region = Region.where(id: params[:id]).select(:id, :code, :name, :description).first
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/region.rb
+++ app/models/region.rb
@@ -2,10 +2,11 @@ class Region < CommonField
   has_many :warehouses, dependent: :restrict_with_error
   has_many :sales_promotion_girls, through: :warehouses
   has_many :supervisors, through: :warehouses
+  has_one :warehouse_relation, -> {select("id")}, class_name: "Warehouse"
 
   before_validation :upcase_code
 
-  validates :code, uniqueness: true
+  #  validates :code, uniqueness: true # tidak menggunakan ini untuk mempercepat proses
   validate :code_not_changed
 
   private
@@ -15,6 +16,6 @@ class Region < CommonField
   end
   
   def code_not_changed
-    errors.add(:code, "change is not allowed!") if code_changed? && persisted? && warehouses.present?
+    errors.add(:code, "change is not allowed!") if code_changed? && persisted? && warehouse_relation.present?
   end
 end
