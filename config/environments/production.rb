cf83a3a13f5ccf9ff698852f278fc3338eef7e07
Yopi Agung Saputra <bombertroop@gmail.com>
2016-07-04 06:16:04 +0700

    changed default url options for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'newposwinaya.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'duos.herokuapp.com' }
 end
