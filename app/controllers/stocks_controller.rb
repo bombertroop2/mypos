26f9805abeed152612f6c285c9d05a69ab56058c
Yopi Agung Saputra <bombertroop@gmail.com>
2016-07-31 10:29:17 +0700

    refactored query on stock module

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,7 +4,7 @@ class StocksController < ApplicationController
   # GET /stocks
   # GET /stocks.json
   def index
-    @stocks = Stock.all
+    @stocks = Stock.select(:id, :warehouse_id)
   end
 
   # GET /stocks/1
