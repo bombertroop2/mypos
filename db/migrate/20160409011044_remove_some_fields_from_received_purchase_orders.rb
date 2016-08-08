8dfb4a0991e8e6595cfc2d6bb43f2627fb89e806
Yopi Agung Saputra <bombertroop@gmail.com>
2016-08-08 21:18:43 +0700

    added rescue nil on remove reference color method call

--- db/migrate/20160409011044_remove_some_fields_from_received_purchase_orders.rb
+++ db/migrate/20160409011044_remove_some_fields_from_received_purchase_orders.rb
@@ -1,6 +1,6 @@
 class RemoveSomeFieldsFromReceivedPurchaseOrders < ActiveRecord::Migration
   def change
     remove_reference :received_purchase_orders, :purchase_order_product, index: true, foreign_key: true
-    remove_reference :received_purchase_orders, :color, index: true, foreign_key: true
+    remove_reference :received_purchase_orders, :color, index: true, foreign_key: true rescue nil
   end
 end
