caa944a175bda11189c61ce4deeea6ec7c47a4f7
Yopi Agung Saputra <bombertroop@gmail.com>
2016-08-08 20:51:43 +0700

    remove some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,56 +12,56 @@ if User.with_any_role(:admin).blank?
   user.add_role :admin
 end
 
-if Brand.count < 1
-  Brand.create name: "Adidas", code: "AD"
-  Brand.create name: "Nike", code: "NK"
-  Brand.create name: "Puma", code: "PM"
-end
-
-if Color.count < 1
-  Color.create name: "White/Black", code: "WHB"
-end
-
-if Model.count < 1
-  Model.create name: "Running", code: "RNL"
-  Model.create name: "Footwear", code: "FTW"
-  Model.create name: "Apparel", code: "APR"
-end
-
-if GoodsType.count < 1
-  GoodsType.create name: "Not Available", code: "NA"
-end
-
-if Region.count < 1
-  Region.create name: "Jabodetabek", code: "JBK"
-end
-
-if PriceCode.count < 1
-  PriceCode.create name: "Price Jakarta", code: "JKT"
-  PriceCode.create name: "Price Luar Jakarta", code: "NJK"
-end
+#if Brand.count < 1
+#  Brand.create name: "Adidas", code: "AD"
+#  Brand.create name: "Nike", code: "NK"
+#  Brand.create name: "Puma", code: "PM"
+#end
+#
+#if Color.count < 1
+#  Color.create name: "White/Black", code: "WHB"
+#end
+#
+#if Model.count < 1
+#  Model.create name: "Running", code: "RNL"
+#  Model.create name: "Footwear", code: "FTW"
+#  Model.create name: "Apparel", code: "APR"
+#end
+#
+#if GoodsType.count < 1
+#  GoodsType.create name: "Not Available", code: "NA"
+#end
+#
+#if Region.count < 1
+#  Region.create name: "Jabodetabek", code: "JBK"
+#end
+#
+#if PriceCode.count < 1
+#  PriceCode.create name: "Price Jakarta", code: "JKT"
+#  PriceCode.create name: "Price Luar Jakarta", code: "NJK"
+#end
 
 #if Vendor.count < 1
 #  Vendor.create name: "Delami", code: "DLM01", address: "Jakarta", terms_of_payment: 90, value_added_tax: "exclude"
 #end
 
-if SizeGroup.count < 1
-  SizeGroup.create code: "A"
-end
-
-if Size.count < 1
-  size_group = SizeGroup.first
-  Size.create size: "6", size_group_id: size_group.id
-  Size.create size: "6-", size_group_id: size_group.id
-  Size.create size: "7", size_group_id: size_group.id
-  Size.create size: "7-", size_group_id: size_group.id
-  Size.create size: "8", size_group_id: size_group.id
-end
-
-if Supervisor.count < 1
-  Supervisor.create code: "ACN", name: "Acien", address: "Jakarta", email: "acien@gmail.com"
-end
-
-if Warehouse.count < 1
-  Warehouse.create code: "GDP", name: "Gudang Pusat", address: "Jakarta", is_active: true, supervisor_id: Supervisor.first.id, region_id: Region.first.id, price_code_id: PriceCode.first.id, warehouse_type: "central"
-end
+#if SizeGroup.count < 1
+#  SizeGroup.create code: "A"
+#end
+#
+#if Size.count < 1
+#  size_group = SizeGroup.first
+#  Size.create size: "6", size_group_id: size_group.id
+#  Size.create size: "6-", size_group_id: size_group.id
+#  Size.create size: "7", size_group_id: size_group.id
+#  Size.create size: "7-", size_group_id: size_group.id
+#  Size.create size: "8", size_group_id: size_group.id
+#end
+#
+#if Supervisor.count < 1
+#  Supervisor.create code: "ACN", name: "Acien", address: "Jakarta", email: "acien@gmail.com"
+#end
+#
+#if Warehouse.count < 1
+#  Warehouse.create code: "GDP", name: "Gudang Pusat", address: "Jakarta", is_active: true, supervisor_id: Supervisor.first.id, region_id: Region.first.id, price_code_id: PriceCode.first.id, warehouse_type: "central"
+#end
