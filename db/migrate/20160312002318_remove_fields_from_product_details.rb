266fde14e9dd3a7140f8464fce9397a7a8d0e5d4
Yopi Agung Saputra <bombertroop@gmail.com>
2016-08-08 21:11:18 +0700

    change association name from color to common_field in order to remove reference from STI model

--- db/migrate/20160312002318_remove_fields_from_product_details.rb
+++ db/migrate/20160312002318_remove_fields_from_product_details.rb
@@ -1,6 +1,6 @@
 class RemoveFieldsFromProductDetails < ActiveRecord::Migration
   def change
-    remove_reference :product_details, :color, index: true, foreign_key: true
+    remove_reference :product_details, :common_field, column: :color_id, index: true, foreign_key: true
     remove_reference :product_details, :product_price_code, index: true, foreign_key: true if ActiveRecord::Base.connection.table_exists? 'product_price_codes'
   end
 end
