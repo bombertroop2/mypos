96ea9ee7b552844d1f3b0ad9741c39d0558cb833
Yopi Agung Saputra <bombertroop@gmail.com>
2017-03-10 08:30:17 +0700

    implemented smart listing on stock module

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,10 +1,25 @@
+include SmartListing::Helper::ControllerExtensions
 class StocksController < ApplicationController
+  helper SmartListing::Helper
   before_action :set_stock, only: [:show, :edit, :update, :destroy]
 
   # GET /stocks
   # GET /stocks.json
   def index
-    @stocks = Stock.select(:id, :warehouse_id)
+    like_command = if Rails.env.eql?("production")
+      "ILIKE"
+    else
+      "LIKE"
+    end
+    stocks_scope = StockDetail.joins(:size, :color, stock_product: [{product: :brand}, {stock: :warehouse}]).select("warehouses.name, products.code, brands_products.name AS brand_name, common_fields.name AS color_name, size AS size_label, quantity")
+    stocks_scope = stocks_scope.where(["warehouses.name #{like_command} ?", "%"+params[:filter]+"%"]).
+      or(stocks_scope.where(["products.code #{like_command} ?", "%"+params[:filter]+"%"])).
+      or(stocks_scope.where(["brands_products.name #{like_command} ?", "%"+params[:filter]+"%"])).
+      or(stocks_scope.where(["common_fields.name #{like_command} ?", "%"+params[:filter]+"%"])).
+      or(stocks_scope.where(["size #{like_command} ?", "%"+params[:filter]+"%"])).
+      or(stocks_scope.where(["quantity #{like_command} ?", "%"+params[:filter]+"%"])) if params[:filter]
+    @stocks = smart_listing_create(:stocks, stocks_scope, partial: 'stocks/listing', default_sort: {:"warehouses.name" => "asc"})
+
   end
 
   # GET /stocks/1
@@ -62,13 +77,13 @@ class StocksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_stock
-      @stock = Stock.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_stock
+    @stock = Stock.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def stock_params
-      params[:stock]
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def stock_params
+    params[:stock]
+  end
 end
