34f56e8d932e1100a0f7a4c4b4821ebff8ce2a91
Yopi Agung Saputra <bombertroop@gmail.com>
2017-04-30 15:51:05 +0700

    cegah user memasukkan data yang salah lewat inspect element ketika menambahkan record baru di email module

--- app/models/email.rb
+++ app/models/email.rb
@@ -8,9 +8,18 @@ class Email < ApplicationRecord
 
   validates :address, :email_type, presence: true
   validates :address, uniqueness: true
+  validate :type_available
   
   def self.account_payable_officers
     where("email_type = 'Account Payable Officer' OR email_type = 'All'").select(:address)
+  end
+  
+  private
+  
+  def type_available
+    TYPES.select{ |x| x[1] == email_type }.first.first
+  rescue
+    errors.add(:email_type, "does not exist!") if email_type.present?
   end
 
 end
