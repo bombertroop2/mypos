943583dfdbd2871a9e8cb2957efe56425bee2858
Yopi Agung Saputra <bombertroop@gmail.com>
2017-05-18 01:44:44 +0700

    modified CreateOrderBookings migrate file

--- db/migrate/20170430225743_create_order_bookings.rb
+++ db/migrate/20170430225743_create_order_bookings.rb
@@ -2,12 +2,14 @@ class CreateOrderBookings < ActiveRecord::Migration[5.0]
   def change
     create_table :order_bookings do |t|
       t.date :plan_date
-      t.references :origin_warehouse, foreign_key: true
-      t.references :destination_warehouse, foreign_key: true
+      t.references :origin_warehouse, index: true
+      t.references :destination_warehouse, index: true
       t.date :print_date
       t.text :note
 
       t.timestamps
     end
+    add_foreign_key :order_bookings, :warehouses, column: :origin_warehouse_id
+    add_foreign_key :order_bookings, :warehouses, column: :destination_warehouse_id
   end
 end
