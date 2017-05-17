d81701ac014b1b1ea651f4321d73fc013ddfdbf7
Yopi Agung Saputra <bombertroop@gmail.com>
2017-05-18 01:48:24 +0700

    modified CreateOrderBookingProductItems migrate file

--- db/migrate/20170501032944_create_order_booking_product_items.rb
+++ db/migrate/20170501032944_create_order_booking_product_items.rb
@@ -3,10 +3,11 @@ class CreateOrderBookingProductItems < ActiveRecord::Migration[5.0]
     create_table :order_booking_product_items do |t|
       t.references :order_booking_product, foreign_key: true
       t.references :size, foreign_key: true
-      t.references :color, foreign_key: true
+      t.references :color, index: true
       t.integer :quantity
 
       t.timestamps
     end
+    add_foreign_key :order_booking_product_items, :common_fields, column: :color_id
   end
 end
