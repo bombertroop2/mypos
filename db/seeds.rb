a0cb75c2adf493754757f0c27b2dd63faa2e342e
Yopi Agung Saputra <bombertroop@gmail.com>
2017-06-14 19:04:57 +0700

    added seed for admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,9 @@
 
 
 if User.with_any_role(:admin).blank?
-  user = User.create email: "rizkinoorlaksana@gmail.com", password: "admin123"
-  user.add_role :admin
+  user = User.new email: "rizkinoorlaksana@gmail.com", password: "admin123", name: "Admin",
+    username: "admin", active: true, role: "admin"
+  user.save validate: false
 end
 
 #if Brand.count < 1
