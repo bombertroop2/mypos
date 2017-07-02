9948be0f8bb3bf2927565f37017f6e3d940e42c7
Yopi Agung Saputra <bombertroop@gmail.com>
2017-07-02 21:09:13 +0700

    tambahkan validasi untuk cek available quantity di gudang ketika mutasi stok

--- app/models/stock_mutation_product_item.rb
+++ app/models/stock_mutation_product_item.rb
@@ -8,13 +8,19 @@ class StockMutationProductItem < ApplicationRecord
   validates :size_id, :color_id, :quantity, presence: true
   validates :quantity, numericality: {greater_than_or_equal_to: 1, only_integer: true}, if: proc { |dpd| dpd.quantity.present? }
 
-  validate :item_available
+    validate :item_available
+    validate :quantity_valid
   
-  private
+    private
   
-  def item_available
-    errors.add(:base, "Some products do not exist!") if StockDetail.joins(stock_product: :stock).
-      where(["warehouse_id = ? AND size_id = ? AND color_id = ? AND stock_products.product_id = ?", origin_warehouse_id, size_id, color_id, product_id]).
-      select("1 AS one").blank?
+    def item_available
+      @stock = StockDetail.joins(stock_product: :stock).
+        where(["warehouse_id = ? AND size_id = ? AND color_id = ? AND stock_products.product_id = ?", origin_warehouse_id, size_id, color_id, product_id]).
+        select(:quantity).first
+      errors.add(:base, "Some products do not exist!") if @stock.blank?
+    end
+  
+    def quantity_valid
+      errors.add(:quantity, "cannot be greater than #{@stock.quantity}") if quantity > @stock.quantity
+    end
   end
-end
