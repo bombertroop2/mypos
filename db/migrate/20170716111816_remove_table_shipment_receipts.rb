4428d9151512b811f09d1c09191ba4776f2b6b4b
Yopi Agung Saputra <bombertroop@gmail.com>
2017-07-16 18:31:35 +0700

    remove shipment receipt product dan shipment receipt product item models

--- db/migrate/20170716111816_remove_table_shipment_receipts.rb
+++ db/migrate/20170716111816_remove_table_shipment_receipts.rb
@@ -1,5 +1,7 @@
 class RemoveTableShipmentReceipts < ActiveRecord::Migration[5.0]
   def change
+    drop_table :shipment_receipt_product_items if ActiveRecord::Base.connection.table_exists? 'shipment_receipt_product_items'
+    drop_table :shipment_receipt_products if ActiveRecord::Base.connection.table_exists? 'shipment_receipt_products'
     drop_table :shipment_receipts if ActiveRecord::Base.connection.table_exists? 'shipment_receipts'
   end
 end
