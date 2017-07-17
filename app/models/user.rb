2bf022e7cb18305c78ddbf24bd775b50f5a4d82e
Yopi Agung Saputra <bombertroop@gmail.com>
2017-07-17 08:12:30 +0700

    jangan audit user apabila login

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   
   attr_accessor :role, :login, :creating_spg_user, :updating_spg_user#:spg_role
        
-  audited on: [:create, :update], except: ["current_sign_in_at", "last_sign_in_at", "current_sign_in_ip", "last_sign_in_ip", "sign_in_count"]
+  audited on: [:create, :update], except: ["current_sign_in_at", "last_sign_in_at", "current_sign_in_ip", "last_sign_in_ip", "sign_in_count", "current_sign_in_token"]
   has_associated_audits
 
   belongs_to :sales_promotion_girl
