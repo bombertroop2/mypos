7b6ee4367ffff21cd8f66d6cea7e96b3d0ebcb83
Yopi Agung Saputra <bombertroop@gmail.com>
2017-08-13 15:43:16 +0700

    add_foreign_key untuk color di beginning_stock_product_details

--- db/migrate/20170803084758_create_beginning_stock_product_details.rb
+++ db/migrate/20170803084758_create_beginning_stock_product_details.rb
@@ -2,7 +2,7 @@ class CreateBeginningStockProductDetails < ActiveRecord::Migration[5.0]
   def change
     create_table :beginning_stock_product_details do |t|
       t.integer :beginning_stock_product_id
-      t.references :color, foreign_key: true
+      t.references :color, index: true
       t.references :size, foreign_key: true
       t.integer :quantity
 
@@ -10,5 +10,6 @@ class CreateBeginningStockProductDetails < ActiveRecord::Migration[5.0]
     end
     add_foreign_key :beginning_stock_product_details, :beginning_stock_products, column: :beginning_stock_product_id
     add_index :beginning_stock_product_details, :beginning_stock_product_id, name: "index_bspd_on_beginning_stock_product_id"
+    add_foreign_key :beginning_stock_product_details, :common_fields, column: :color_id
   end
 end
