c7198e5dcd4b04920ea7b30638f10191b76fdc59
Yopi Agung Saputra <bombertroop@gmail.com>
2017-08-17 19:15:06 +0700

    jangan tampilkan seluruh stock di lisintg stock balance, wajib filter berdasarkan warehouse

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -2,9 +2,13 @@ $(function () {
     $("#warehouse_id").attr("data-placeholder", "Please select warehouse").chosen();
     $("#warehouse_id_chosen").addClass("text-left");
     $("#search-stock-btn").click(function () {
-        $("#filter_warehouse_id").val($("#warehouse_id").val());
-        $("#filter_product_criteria").val($("#filter-product-criteria").val());
-        $(".smart-listing-controls").submit();
+        if ($("#warehouse_id").val() !== undefined && $("#warehouse_id").val() == "")
+            bootbox.alert({message: "Please select warehouse first!", size: "small"});
+        else {
+            $("#filter_warehouse_id").val($("#warehouse_id").val());
+            $("#filter_product_criteria").val($("#product_criteria").val());
+            $(".smart-listing-controls").submit();
+        }
     });
 
 });
