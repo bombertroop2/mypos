0a12a4dc4952210de6b82ee8d38d71769a33c620
Yopi Agung Saputra <bombertroop@gmail.com>
2017-08-18 08:39:00 +0700

    merging baris table listing stocks

--- app/controllers/listing_stocks_controller.rb
+++ app/controllers/listing_stocks_controller.rb
@@ -19,7 +19,29 @@ class ListingStocksController < ApplicationController
     else
       listing_stock_transactions_scope = ListingStockTransaction.none
     end
-    @listing_stock_transactions = smart_listing_create(:listing_stock_transactions, listing_stock_transactions_scope, partial: 'listing_stocks/listing', paginate: false, default_sort: {transaction_date: "ASC", product_code: "ASC"})
+    
+    listing_stock_transactions_scope = if params[:listing_stock_transactions_smart_listing].present? && params[:listing_stock_transactions_smart_listing][:sort].present?
+      order_fields = if params[:listing_stock_transactions_smart_listing][:sort].keys.first.eql?("transaction_date")
+        "#{params[:listing_stock_transactions_smart_listing][:sort].keys.first} #{params[:listing_stock_transactions_smart_listing][:sort][params[:listing_stock_transactions_smart_listing][:sort].keys.first]}, transaction_number ASC, transaction_type ASC, products.code ASC, common_fields.code ASC, size_order ASC"
+      elsif params[:listing_stock_transactions_smart_listing][:sort].keys.first.eql?("transaction_number")
+        "transaction_date ASC, #{params[:listing_stock_transactions_smart_listing][:sort].keys.first} #{params[:listing_stock_transactions_smart_listing][:sort][params[:listing_stock_transactions_smart_listing][:sort].keys.first]}, transaction_type ASC, products.code ASC, common_fields.code ASC, size_order ASC"
+      elsif params[:listing_stock_transactions_smart_listing][:sort].keys.first.eql?("transaction_type")
+        "transaction_date ASC, transaction_number ASC, #{params[:listing_stock_transactions_smart_listing][:sort].keys.first} #{params[:listing_stock_transactions_smart_listing][:sort][params[:listing_stock_transactions_smart_listing][:sort].keys.first]}, products.code ASC, common_fields.code ASC, size_order ASC"
+      elsif params[:listing_stock_transactions_smart_listing][:sort].keys.first.eql?("products.code")
+        "transaction_date ASC, transaction_number ASC, transaction_type ASC, #{params[:listing_stock_transactions_smart_listing][:sort].keys.first} #{params[:listing_stock_transactions_smart_listing][:sort][params[:listing_stock_transactions_smart_listing][:sort].keys.first]}, common_fields.code ASC, size_order ASC"
+      elsif params[:listing_stock_transactions_smart_listing][:sort].keys.first.eql?("common_fields.code")
+        "transaction_date ASC, transaction_number ASC, transaction_type ASC, products.code ASC, #{params[:listing_stock_transactions_smart_listing][:sort].keys.first} #{params[:listing_stock_transactions_smart_listing][:sort][params[:listing_stock_transactions_smart_listing][:sort].keys.first]}, size_order ASC"
+      elsif params[:listing_stock_transactions_smart_listing][:sort].keys.first.eql?("size_order")
+        "transaction_date ASC, transaction_number ASC, transaction_type ASC, products.code ASC, common_fields.code ASC, #{params[:listing_stock_transactions_smart_listing][:sort].keys.first} #{params[:listing_stock_transactions_smart_listing][:sort][params[:listing_stock_transactions_smart_listing][:sort].keys.first]}"
+      elsif params[:listing_stock_transactions_smart_listing][:sort].keys.first.eql?("quantity")
+        "transaction_date ASC, transaction_number ASC, transaction_type ASC, products.code ASC, common_fields.code ASC, size_order ASC, #{params[:listing_stock_transactions_smart_listing][:sort].keys.first} #{params[:listing_stock_transactions_smart_listing][:sort][params[:listing_stock_transactions_smart_listing][:sort].keys.first]}"
+      end
+      listing_stock_transactions_scope.order(order_fields)
+    else
+      listing_stock_transactions_scope.order("transaction_date ASC, transaction_number ASC, transaction_type ASC, products.code ASC, common_fields.code ASC, size_order ASC")
+    end
+
+    @listing_stock_transactions = smart_listing_create(:listing_stock_transactions, listing_stock_transactions_scope, partial: 'listing_stocks/listing', paginate: false, sort: {transaction_date: "ASC"})
   end
 
   # GET /listing_stocks/1
