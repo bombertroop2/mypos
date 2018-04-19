59ec1ea4e8f4f64bfdbc9857a0a161db0fde03cf
Yopi Agung Saputra <bombertroop@gmail.com>
2018-04-19 20:27:48 +0700

    hapus migrasi untuk menambahkan index ke table shipment_receipts (tablenya gaada)

--- db/migrate/20170628062454_add_unique_index_to_shipment_receipt_product_items.rb
+++ db/migrate/20170628062454_add_unique_index_to_shipment_receipt_product_items.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToShipmentReceiptProductItems < ActiveRecord::Migration[5.0]
   def change
-    add_index :shipment_receipt_product_items, [:shipment_receipt_product_id, :shipment_product_item_id], unique: true, name: "index_srpi_on_srp_id_and_spi_id"
+#    add_index :shipment_receipt_product_items, [:shipment_receipt_product_id, :shipment_product_item_id], unique: true, name: "index_srpi_on_srp_id_and_spi_id"
   end
 end
--- db/migrate/20170628172220_add_index_to_shipment_id_on_shipment_receipts.rb
+++ db/migrate/20170628172220_add_index_to_shipment_id_on_shipment_receipts.rb
@@ -1,6 +1,6 @@
 class AddIndexToShipmentIdOnShipmentReceipts < ActiveRecord::Migration[5.0]
   def change
-    remove_index :shipment_receipts, :shipment_id if index_exists?(:shipment_receipts, :shipment_id)
-    add_index :shipment_receipts, :shipment_id, unique: true
+#    remove_index :shipment_receipts, :shipment_id if index_exists?(:shipment_receipts, :shipment_id)
+#    add_index :shipment_receipts, :shipment_id, unique: true
   end
 end
