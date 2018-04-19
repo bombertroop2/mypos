cd52eae543df32d522a447355d92cd61db3fa2c2
Yopi Agung Saputra <bombertroop@gmail.com>
2018-04-19 20:20:37 +0700

    hapus migrasi untuk menambahkan quantity ke table shipment_receipts (tablenya gaada)

--- db/migrate/20170627080256_add_quantity_to_shipment_receipts.rb
+++ db/migrate/20170627080256_add_quantity_to_shipment_receipts.rb
@@ -1,5 +1,5 @@
 class AddQuantityToShipmentReceipts < ActiveRecord::Migration[5.0]
   def change
-    add_column :shipment_receipts, :quantity, :integer
+#    add_column :shipment_receipts, :quantity, :integer
   end
 end
