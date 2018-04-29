2086af279e07d4a11a5ce821f540f41b763a2bab
febbryramadhan <febbryramadhan17@gmail.com>
2018-04-29 23:09:13 +0700

    update counter events

--- app/models/counter_event.rb
+++ app/models/counter_event.rb
@@ -1,7 +1,10 @@
 class CounterEvent < ApplicationRecord
+	validates :code, :name, :start_time, :end_time, presence: true
+
 	has_many :counter_event_warehouses
 	
 	def set_warehouses(w_ids)
+		puts "w_ids #{w_ids}"
 		counter_event_warehouses.destroy_all
 		warehouse_ids = w_ids.split(", ")
 		arr = warehouse_ids.map {|x| {counter_event_id: id, warehouse_id: x}}
