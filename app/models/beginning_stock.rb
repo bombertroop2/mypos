bf4113a8ab5acca1e54e69bd1cb98b93e2913fb8
Yopi Agung Saputra <bombertroop@gmail.com>
2018-05-12 06:03:56 +0700

    tambahkan warehouses.is_active = true di beberapa query di modul beginning stock

--- app/models/beginning_stock.rb
+++ app/models/beginning_stock.rb
@@ -1,4 +1,12 @@
 class BeginningStock < ApplicationRecord
   belongs_to :warehouse
   has_many :beginning_stock_months, dependent: :destroy
+  
+  validate :warehouse_is_active
+  
+  private
+  
+  def warehouse_is_active
+    errors.add(:base, "Sorry, warehouse is not active") if Warehouse.select("1 AS one").where(id: warehouse_id).where(["warehouses.is_active = ?", true]).blank?
+  end
 end
