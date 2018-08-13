5f4c6971374cbab5fa48cc69b88156cb1956c21e
Yopi Agung Saputra <bombertroop@gmail.com>
2018-08-13 15:46:10 +0700

    setting actioncable di production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -72,9 +72,11 @@ $(window).on('load', function () {
     App.notifications = App.cable.subscriptions.create("NotificationsChannel", {
         connected: function () {
 // Called when the subscription is ready for use on the server
+            console.log("connected");
         },
         disconnected: function () {
             // Called when the subscription has been terminated by the server
+            console.log("disconnected");
         },
         received: function (data) {
             if ($(".notification-rows").length > 3)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://103.18.133.198/cable'
+  config.action_cable.allowed_request_origins = [ 'http://103.18.133.198', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
