8f549946fe9d06698ebedad502a498ebcadc809d
Yopi Agung Saputra <bombertroop@gmail.com>
2018-08-14 15:09:43 +0700

    hapus beberapa header tambahan dari dalam blok cable di nginx.conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'ws://103.18.133.198/cable'
   config.action_cable.allowed_request_origins = [ 'http://103.18.133.198', /http:\/\/example.*/ ]
-
+  config.action_cable.disable_request_forgery_protection = true
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
@@ -100,5 +100,5 @@ Rails.application.configure do
   
   #  config.web_socket_server_url = "wss://duos.herokuapp.com/cable"
 
-#  config.action_cable.allowed_request_origins = [ 'https://one-s.herokuapp.com', 'http://one-s.herokuapp.com' ]
+  #  config.action_cable.allowed_request_origins = [ 'https://one-s.herokuapp.com', 'http://one-s.herokuapp.com' ]
 end
