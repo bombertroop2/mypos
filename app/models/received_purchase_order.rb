d924c309e8ce8443ccbd802ff0eb84b5261b6a61
agung43 <sniveragung43@gmail.com>
2018-08-20 17:47:11 +0700

    purchase order journal and return pos journal

--- app/models/received_purchase_order.rb
+++ app/models/received_purchase_order.rb
@@ -5,12 +5,12 @@ class ReceivedPurchaseOrder < ApplicationRecord
   belongs_to :direct_purchase
   belongs_to :vendor
   has_many :received_purchase_order_products, dependent: :destroy
-  
-  
+
+
   accepts_nested_attributes_for :received_purchase_order_products, reject_if: :child_blank
-  
+
   before_validation :strip_string_values, if: proc{|rpo| rpo.is_using_delivery_order.eql?("yes")}, on: :create
-      
+
     validates :delivery_order_number, presence: true, unless: proc{|rpo| rpo.is_using_delivery_order.eql?("no")}, on: :create
       validate :minimum_receiving_item, on: :create, unless: proc {|rpo| rpo.is_it_direct_purchasing}
         validates :receiving_date, presence: true, on: :create, unless: proc {|rpo| rpo.is_it_direct_purchasing}
@@ -23,30 +23,76 @@ class ReceivedPurchaseOrder < ApplicationRecord
 
                       before_create :create_auto_do_number, if: proc{|rpo| rpo.is_using_delivery_order.eql?("no")}
                         before_create :calculate_total_quantity, unless: proc{|rpo| rpo.is_it_direct_purchasing}
-      
+                        after_create :purchase_order_journal, unless: proc{|rpo| rpo.is_it_direct_purchasing}
+
                           attr_accessor :is_it_direct_purchasing
-  
+
                           private
-                        
-                          def transaction_open                            
+
+                          def transaction_open
                             errors.add(:base, "Sorry, you can't perform this transaction") if FiscalYear.joins(:fiscal_months).where(year: receiving_date.year).where("fiscal_months.month = '#{Date::MONTHNAMES[receiving_date.month]}' AND fiscal_months.status = 'Close'").select("1 AS one").present?
                           end
-                    
+
                           def strip_string_values
                             self.delivery_order_number = delivery_order_number.strip
                           end
-                  
+
                           def purchase_order_receivable
                             errors.add(:base, "Not able to receive selected PO") unless PurchaseOrder.select("1 AS one").where("(status = 'Open' OR status = 'Partial') AND id = '#{purchase_order_id}' AND vendor_id = '#{vendor_id}'").present?
                           end
-                
+
                           def calculate_total_quantity
                             self.quantity = 0
                             received_purchase_order_products.each do |received_purchase_order_product|
                               self.quantity += received_purchase_order_product.received_purchase_order_items.map(&:quantity).sum
                             end
                           end
-    
+
+                          def purchase_order_journal
+                            coa = Coa.find_by_transaction_type("PO")
+                            gross_price = self.purchase_order.get_gross_price
+                            warehouse = self.purchase_order.warehouse_id
+                            transaction_date = self.purchase_order.purchase_order_date
+                            if self.purchase_order.first_discount.present? && self.purchase_order.second_discount.present?
+                              first_discount = gross_price * self.purchase_order.first_discount/100
+                              second_discount = (gross_price - first_discount) * self.purchase_order.second_discount/100
+                              discount = first_discount + second_discount
+                            elsif self.purchase_order.first_discount.present?
+                              discount = gross_price * self.purchase_order.first_discount/100
+                            else
+                              discount = 0
+                            end
+                            gross_after_discount = gross_price - discount
+                            ppn = gross_after_discount * 10 / 100
+                            nett = gross_after_discount - ppn
+                            if self.purchase_order.journal.present?
+                              self.purchase_order.journal.update(
+                                                  coa_id: coa.id,
+                                                  gross: gross_price.to_f,
+                                                  gross_after_discount: gross_after_discount.to_f,
+                                                  discount: discount.to_f,
+                                                  ppn: ppn.to_f,
+                                                  nett: nett.to_f,
+                                                  transaction_date: transaction_date,
+                                                  activity: nil,
+                                                  warehouse_id:warehouse
+                                                )
+                            else
+                              journal = self.purchase_order.build_journal(
+                                                  coa_id: coa.id,
+                                                  gross: gross_price.to_f,
+                                                  gross_after_discount: gross_after_discount.to_f,
+                                                  discount: discount.to_f,
+                                                  ppn: ppn.to_f,
+                                                  nett: nett.to_f,
+                                                  transaction_date: transaction_date,
+                                                  activity: nil,
+                                                  warehouse_id:warehouse
+                                                )
+                              journal.save
+                            end
+                          end
+
                           def create_auto_do_number
                             company_code = Company.pluck(:code).first
                             today = Date.current
@@ -69,7 +115,7 @@ class ReceivedPurchaseOrder < ApplicationRecord
                                   seq_number = existed_number.delivery_order_number.split("#{company_code}#{(vendor.code)}#{current_month}#{current_year}").last
                                   if seq_number.to_i > 1 && index == 0
                                     new_number = "#{company_code}#{(vendor.code)}#{current_month}#{current_year}001"
-                                    break                              
+                                    break
                                   elsif last_seq_number.eql?("")
                                     last_seq_number = seq_number
                                   elsif (seq_number.to_i - last_seq_number.to_i) > 1
@@ -81,21 +127,21 @@ class ReceivedPurchaseOrder < ApplicationRecord
                                     last_seq_number = seq_number
                                   end
                                 end
-                              end                        
+                              end
                             end
 
                             self.delivery_order_number = new_number
                           end
-    
+
                           def minimum_receiving_item
                             errors.add(:base, "Please insert at least one item") if received_purchase_order_products.blank?
                           end
-    
+
                           def child_blank(attributed)
-                            attributed[:received_purchase_order_items_attributes].each do |key, value| 
+                            attributed[:received_purchase_order_items_attributes].each do |key, value|
                               return false if value[:quantity].present?
                             end
-      
+
                             return true
                           end
 
--- app/models/sales_return.rb
+++ app/models/sales_return.rb
@@ -3,24 +3,48 @@ class SalesReturn < ApplicationRecord
   belongs_to :returned_receipt, class_name: "Sale", foreign_key: "sale_id"
   has_one :sale
   has_many :sales_return_products, dependent: :destroy
+  has_one :journal, :as => :transactionable
 
   accepts_nested_attributes_for :sales_return_products
   accepts_nested_attributes_for :sale
-  
-  before_validation :get_sale 
+
+  before_validation :get_sale
   validate :is_new_sale_exist, :is_returned_product_exist
   before_create :generate_number, :calculate_total_return, :calculate_total_return_quantity
-  
+  after_create :pos_return_journal
+
   private
-  
+
+  def pos_return_journal
+    coa = Coa.find_by_transaction_type('RET')
+    gross_price = self.sale.sale_products.collect{|x| x.price_list.price.to_i }.sum
+    discount = gross_price-self.total_return
+    gross_after_discount = self.total_return
+    ppn = gross_after_discount * 10 / 100
+    nett = gross_after_discount - ppn
+    warehouse = self.sale.cashier_opening.warehouse.id
+    journal = self.build_journal(
+        coa_id: coa.id,
+        gross: gross_price.to_f,
+        gross_after_discount: self.total_return.to_f,
+        discount: discount.to_f,
+        ppn: ppn.to_f,
+        nett: nett.to_f,
+        transaction_date: self.sale.transaction_time.to_date,
+        activity: nil,
+        warehouse_id: warehouses
+      )
+    journal.save
+  end
+
   def calculate_total_return_quantity
     self.total_return_quantity = sales_return_products.length
   end
-  
+
   def calculate_total_return
     self.total_return = SaleProduct.where(id: sales_return_products.map(&:sale_product_id)).sum(:total)
   end
-  
+
   def get_sale
     @sale = Sale.
       joins(cashier_opening: [warehouse: :sales_promotion_girls]).
@@ -33,15 +57,15 @@ class SalesReturn < ApplicationRecord
       @sale = nil
     end
   end
-  
+
   def is_returned_product_exist
     errors.add(:base, "Sales return must have at least one return product") if sales_return_products.blank?
-  end  
+  end
 
   def is_new_sale_exist
     errors.add(:base, "Sales return must have at least one receipt") if sale.blank?
-  end  
-  
+  end
+
   def generate_number
     today = Date.current
     current_year = today.strftime("%y").rjust(2, '0')
