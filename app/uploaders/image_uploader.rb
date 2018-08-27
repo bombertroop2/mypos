c818872d88cd628296aacbdf0346b20e69581f30
Yopi Agung Saputra <bombertroop@gmail.com>
2018-08-27 12:42:04 +0700

    ganti storage image_uploader ke file

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,7 +9,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  storage :file
   # storage :gcloud
 
   # storage :fog
