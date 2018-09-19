22bf664037e20baaeb197ac59349fd2f9e7bea24
Yopi Agung Saputra <bombertroop@gmail.com>
2018-09-20 06:52:24 +0700

    fixed issue => reponse 'HTML' ketika login via android

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,6 +1,7 @@
 class Api::SessionsController < Devise::SessionsController
   #  authorize_resource
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, :browser_supported, :configure_permitted_parameters
+  skip_around_action :set_time_zone
 
   def create
     user = User.where(["(lower(username) = :value OR lower(email) = :value) AND active = :active_value", {value: params[:login].strip.downcase, active_value: true}]).first
