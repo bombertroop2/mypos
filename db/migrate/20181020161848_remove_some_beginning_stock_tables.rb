0da27050ee16fb7aad4cae89cf7abb99626ad6bb
Yopi Agung Saputra <bombertroop@gmail.com>
2018-10-21 18:00:53 +0700

    tambahkan force cascade di drop table beginning stock migration file

--- db/migrate/20181020161848_remove_some_beginning_stock_tables.rb
+++ db/migrate/20181020161848_remove_some_beginning_stock_tables.rb
@@ -1,6 +1,6 @@
 class RemoveSomeBeginningStockTables < ActiveRecord::Migration[5.0]
   def change
-    drop_table :beginning_stocks if ActiveRecord::Base.connection.table_exists? 'beginning_stocks'
+    drop_table :beginning_stocks, force: :cascade if ActiveRecord::Base.connection.table_exists? 'beginning_stocks'
     drop_table :beginning_stock_months if ActiveRecord::Base.connection.table_exists? 'beginning_stock_months'
     drop_table :beginning_stock_product_details if ActiveRecord::Base.connection.table_exists? 'beginning_stock_product_details'
   end
