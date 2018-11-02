dd2b9195a41fa70425da1e370f91d392283efedd
Yopi Agung Saputra <bombertroop@gmail.com>
2018-11-02 19:23:18 +0700

    fixed bug => shipment/DO yang belum di print tidak boleh di receive

--- app/controllers/api/shipments_controller.rb
+++ app/controllers/api/shipments_controller.rb
@@ -37,7 +37,7 @@ class Api::ShipmentsController < Api::ApplicationController
     #      Shipment.select(:id).joins(:order_booking).where("order_bookings.destination_warehouse_id = #{current_user.sales_promotion_girl.warehouse_id}").where(["(shipments.delivery_order_number = ? OR order_bookings.number = ?) AND received_date IS NULL", params[:do_ob_number], params[:do_ob_number]])
     #    end
     #    @delivery_orders = Shipment.select(:id, :delivery_order_number).joins(:order_booking).where("order_bookings.destination_warehouse_id = #{current_user.sales_promotion_girl.warehouse_id}").where(["(shipments.delivery_order_number = ? OR order_bookings.number = ?) AND received_date IS NULL", params[:do_number], params[:do_number]])
-    @delivery_orders = Shipment.select(:id, :delivery_order_number).joins(:order_booking).where("order_bookings.destination_warehouse_id = #{current_user.sales_promotion_girl.warehouse_id}").where(["shipments.delivery_order_number = ? AND received_date IS NULL", params[:do_number]])
+    @delivery_orders = Shipment.select(:id, :delivery_order_number).joins(:order_booking).where("order_bookings.destination_warehouse_id = #{current_user.sales_promotion_girl.warehouse_id}").where(["shipments.delivery_order_number = ? AND received_date IS NULL", params[:do_number]]).where(is_document_printed: true)
     
     # apabila jumlahnya lebih dari satu maka artinya satu DO bisa banyak OB, jadi tidak bisa search by OB number (tapi sekarang 1 DO = 1 OB)
     if @delivery_orders.length == 0 || @delivery_orders.length > 1
@@ -59,6 +59,7 @@ class Api::ShipmentsController < Api::ApplicationController
     #    end
     @delivery_order = Shipment.joins(:order_booking).where(id: params[:id]).
       where("order_bookings.destination_warehouse_id = #{current_user.sales_promotion_girl.warehouse_id}").
+      where(is_document_printed: true).
       select("shipments.*").first
   end
 
