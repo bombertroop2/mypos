f4d48822a01a8188e7add516813e1e7becf5e12e
Yopi Agung Saputra <bombertroop@gmail.com>
2018-12-19 10:24:00 +0700

    tambahkan validasi untuk Fiscal

--- app/models/fiscal_year.rb
+++ app/models/fiscal_year.rb
@@ -1,5 +1,5 @@
 class FiscalYear < ApplicationRecord
-  audited
+  audited on: [:create, :update]
   has_associated_audits
 
   has_many :fiscal_months, dependent: :destroy
@@ -7,4 +7,20 @@ class FiscalYear < ApplicationRecord
 
   validates :year, presence: true, uniqueness: true
   validates :year, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: Date.current.year, only_integer: true}, if: proc { |fy| fy.year.present? }
+    
+    before_destroy :next_fiscal_not_existed, :delete_tracks
+    
+    private
+    
+    def next_fiscal_not_existed
+      fy = FiscalYear.select(:year).where(["year > ?", year]).order(:year).first
+      if fy.present?
+        errors.add(:base, "Please delete fiscal #{fy.year} first")
+        throw :abort
+      end
+    end
+    
+    def delete_tracks
+      audits.destroy_all
+    end
   end
