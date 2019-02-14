edc410712077e58aa55427ba72d9db99b98e63f4
Yopi Agung Saputra <bombertroop@gmail.com>
2019-02-14 22:13:41 +0700

    tambahkan kondisi untuk cek table beginning_stocks exist

--- db/migrate/20170805233550_add_unique_index_on_warehouse_id_to_beginning_stocks.rb
+++ db/migrate/20170805233550_add_unique_index_on_warehouse_id_to_beginning_stocks.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexOnWarehouseIdToBeginningStocks < ActiveRecord::Migration[5.0]
   def change
-    add_index :beginning_stocks, :warehouse_id, unique: true, name: "unique_index_beginning_stocks_on_warehouse_id"
+    add_index :beginning_stocks, :warehouse_id, unique: true, name: "unique_index_beginning_stocks_on_warehouse_id" if ActiveRecord::Base.connection.table_exists? 'beginning_stocks'
   end
 end
