ed2d3985af9a9ad872f3161f3de6df63f891a675
Yopi Agung Saputra <bombertroop@gmail.com>
2019-02-14 22:10:19 +0700

    tambahkan kondisi untuk cek table beginning_stock_months exist

--- db/migrate/20170803084611_create_beginning_stock_products.rb
+++ db/migrate/20170803084611_create_beginning_stock_products.rb
@@ -1,7 +1,7 @@
 class CreateBeginningStockProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :beginning_stock_products do |t|
-      t.references :beginning_stock_month, foreign_key: true
+      t.integer :beginning_stock_month_id, index: true
       t.references :product, foreign_key: true
 
       t.timestamps
