0537da98cde497f2c6e70fb0b9f015108c0b4d49
Yopi Agung Saputra <bombertroop@gmail.com>
2019-04-08 00:41:27 +0700

    added rescue block on app/jobs/fix_product_master_job.rb

--- app/jobs/fix_product_master_job.rb
+++ app/jobs/fix_product_master_job.rb
@@ -6,23 +6,28 @@ class FixProductMasterJob < ApplicationJob
   queue_as :default
 
   def perform
-    products = Product.joins(:size_group).where("size_groups.code = 'A'")
-    products.each do |product|
-      product.product_colors.each do |pc|
-        product.product_details.pluck(:size_id).uniq.each do |size_id|
-          product_barcode = ProductBarcode.where(product_color_id: pc.id, size_id: size_id).first
-          if product_barcode.blank?
-            pb = ProductBarcode.where(["barcode LIKE ?", "1S%"]).select(:barcode).order("barcode DESC").first
-            barcode = if pb.present?
-              "1S#{pb.barcode.split("1S")[1].succ}"
-            else
-              "1S00001"
+    begin
+      products = Product.joins(:size_group).where("size_groups.code = 'A'")
+      products.each do |product|
+        product.product_colors.each do |pc|
+          product.product_details.pluck(:size_id).uniq.each do |size_id|
+            product_barcode = ProductBarcode.where(product_color_id: pc.id, size_id: size_id).first
+            if product_barcode.blank?
+              pb = ProductBarcode.where(["barcode LIKE ?", "1S%"]).select(:barcode).order("barcode DESC").first
+              barcode = if pb.present?
+                "1S#{pb.barcode.split("1S")[1].succ}"
+              else
+                "1S00001"
+              end
+              product_barcode = ProductBarcode.new(product_color_id: pc.id, size_id: size_id, barcode: barcode)
+              product_barcode.save
             end
-            product_barcode = ProductBarcode.new(product_color_id: pc.id, size_id: size_id, barcode: barcode)
-            product_barcode.save
           end
         end
       end
     end
+    DuosMailer.import_product_error_email("BERES ANJING").deliver
+  rescue    
+    DuosMailer.import_product_error_email("ERROR GOBLOG").deliver
   end
 end
