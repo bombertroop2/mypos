d60f70f616bb9dc2ef615d58a624e5ee68ab2ba1
Yopi Agung Saputra <bombertroop@gmail.com>
2019-04-18 21:46:05 +0700

    cegah user untuk menambahkan MEMO di master lebih dari satu

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -11,48 +11,49 @@ class Bank < ApplicationRecord
   validates :code, :name, :card_type, presence: true
   validate :card_type_available
   validate :code_not_changed, :type_not_changed
+  validates :code, uniqueness: true, if: proc{|b| b.code.present? && b.code_changed? && b.code.downcase.eql?("memo")}
 
-  before_save :strip_fields
-  before_save :upcase_code
-  before_save :remove_space_from_code
+    before_save :strip_fields
+    before_save :upcase_code
+    before_save :remove_space_from_code
   
-  before_destroy :delete_tracks
+    before_destroy :delete_tracks
   
-  def code_and_name
-    "#{code} - #{name} (#{card_type})"
-  end
+    def code_and_name
+      "#{code} - #{name} (#{card_type})"
+    end
   
-  private
+    private
   
-  def code_not_changed
-    errors.add(:code, "change is not allowed!") if code_changed? && persisted? && sale_relation.present?
-  end
+    def code_not_changed
+      errors.add(:code, "change is not allowed!") if code_changed? && persisted? && sale_relation.present?
+    end
 
-  def type_not_changed
-    errors.add(:card_type, "change is not allowed!") if card_type_changed? && persisted? && sale_relation.present?
-  end
+    def type_not_changed
+      errors.add(:card_type, "change is not allowed!") if card_type_changed? && persisted? && sale_relation.present?
+    end
   
-  def delete_tracks
-    audits.destroy_all
-  end
+    def delete_tracks
+      audits.destroy_all
+    end
   
-  def remove_space_from_code
-    self.code = code.delete(" ")
-  end
+    def remove_space_from_code
+      self.code = code.delete(" ")
+    end
   
-  def upcase_code    
-    self.code = code.upcase.gsub(" ","").gsub("\t","")
-  end
+    def upcase_code    
+      self.code = code.upcase.gsub(" ","").gsub("\t","")
+    end
   
-  def strip_fields
-    self.code = code.strip
-    self.name = name.strip
-  end
+    def strip_fields
+      self.code = code.strip
+      self.name = name.strip
+    end
 
-  def card_type_available
-    TYPES.select{ |x| x[1] == card_type }.first.first
-  rescue
-    errors.add(:card_type, "does not exist!") if card_type.present?
-  end
+    def card_type_available
+      TYPES.select{ |x| x[1] == card_type }.first.first
+    rescue
+      errors.add(:card_type, "does not exist!") if card_type.present?
+    end
 
-end
+  end
